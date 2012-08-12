5370ca92e4d4056ac4f3c1024915365fb851ce83
Irr0 R Irregular <irratio@gmail.com>
2012-08-12 19:38:19 +0600

    regexp here! almost done! (not really)

--- jck2twtr.rb
+++ jck2twtr.rb
@@ -11,11 +11,21 @@ items = doc.css('item')
 
 items.each do |item|
   description = Nokogiri::HTML(item.css('description').text)
+
   description.css('a').each do |a|
     a.replace(a['href'])
   end
-  puts media = (item.xpath('media:content').first || {})['url']
-  puts text = description.xpath("//text()").to_s
-  puts link = item.css('link').text
-  
+
+  media = (item.xpath('media:content').first || {})['url']
+  text = description.xpath("//text()").to_s
+  link = item.css('link').text
+
+  tweet = "#{link} #{media} #{text}".split(' ').map do |word|
+    if word.start_with? 'http'
+      word
+    else
+      word.gsub /[аеиоуыэюяьъ]+([^ .,;:…!?-])/ do |s| $1 end
+    end
+  end.join(' ').gsub(/ +/, ' ')
+  puts tweet
 end
